9c7ad18bc3a129c1ef5c34e5c38b275b5011a8f6
Monali-Khandelwal <khandelwal.monali@gmail.com>
2021-05-10 18:30:24 -0400

    further updates

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,15 @@
-import {createStore, combineReducers} from 'redux';
+import {createStore, combineReducers, applyMiddleware, compose} from 'redux';
+import { productListReducer } from './reducers/productReducers';
+import thunk from 'redux-thunk';
 
 const intitialState = {};
 // reducer is a function which gets state and action and retruns a new state based on the action
-const reducer = combineReducers({
 
+const reducer = combineReducers({
     productList: productListReducer,
 })
-const store = createStore(reducer, initialState);
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, initialState, composeEnhancer(applyMiddleware(thunk)));
+
+export default store;
